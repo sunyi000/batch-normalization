4a69c10cf88ba4c82473ceb8684dc324b30e3ba9
Yi Sun <sunyi000@gmail.com>
2021-02-18 22:14:39 +0100

    updates

---
+++ common.R
@@ -0,0 +1,59 @@
+get_normOfAVector <- function(x1, y1, x2, y2) 
+{
+  return(sqrt((x2-x1)^2+(y2-y1)^2))
+}
+
+getdistance_fromPointToSegment <- function(test.x, test.y, x1, y1, x2, y2) 
+{
+  ## Distance from (test.x,test.y) to the segment x1,y1,x2,y2
+  distance <- NULL
+  intersectingPoint.x <- 0
+  intersectingPoint.y <- 0
+  
+  normOfTheSegment <- get_normOfAVector(x1, y1, x2, y2)
+  if( normOfTheSegment < 0.00000001) 
+  {
+    warning("Short segment")
+    return(9999)
+  }
+  u <- (((test.x - x1) * (x2 - x1)) + ((test.y - y1) * (y2 - y1)))
+  u <- u / (normOfTheSegment * normOfTheSegment)
+  if((u < 0.00001) || (u > 1)) 
+  {
+    ## closest point does not fall within the line segment, take the shorter distance to an endpoint
+    intersectingPoint.x <- get_normOfAVector(test.x, test.y, x1, y1)
+    intersectingPoint.y <- get_normOfAVector(test.x, test.y, x2, y2)
+    if (intersectingPoint.x > intersectingPoint.y)
+    {
+      distance <- intersectingPoint.y
+    } else 
+    {
+      distance <- intersectingPoint.x
+    }
+  } else 
+  {
+    ## Intersecting point is on the line, use the formula
+    intersectingPoint.x <- x1 + u * (x2 - x1)
+    intersectingPoint.y <- y1 + u * (y2 - y1)
+    distance <- get_normOfAVector(test.x, test.y, intersectingPoint.x, intersectingPoint.y)
+  }
+  return(distance)
+}
+
+
+get_elbow_ofDistribution <- function(xPoints, yPoints)
+{
+  xyCoordinates <- data.frame(x=xPoints, y=yPoints)
+  # Points
+  point_upLeft    <- as.vector(xyCoordinates[1,])
+  point_downRight <- as.vector(xyCoordinates[nrow(xyCoordinates),])
+  # Segments
+  diagonalSegment <- point_downRight - point_upLeft
+  diagonalSegmentNormalized <- diagonalSegment/sqrt(sum(diagonalSegment^2))
+  
+  xyCoordinates$distToSegment <- apply(xyCoordinates, 1, function(row) { getdistance_fromPointToSegment(row[1], row[2], point_upLeft[1]$x, point_upLeft[2]$y, point_downRight[1]$x, point_downRight[2]$y)})
+  
+  maxDistanceToTheSegment <- xyCoordinates[which.max(xyCoordinates$distToSegment),]$x
+  return(maxDistanceToTheSegment)
+}
+
--- normalization.R
+++ normalization.R
@@ -1,11 +1,14 @@
 library("dplyr")
 library(sva)
-
+library(janitor)
+library(ggplot2)
+source("common.R")
+require(graphics)
 
 
 ###############################################################
 
-# files <- list.files(path="test-data/dnaDamage-1/", pattern="*._ch-0.txt", full.names=TRUE, recursive=FALSE)
+# files <- list.files(path="features/dnaDamage/", pattern="*._ch-0.txt", full.names=TRUE, recursive=FALSE)
 # for (ch0_file in files){
 #   if (!exists("dataset")){
 #     dataset <- read.table(ch0_file, header=TRUE, sep="\t", stringsAsFactors=FALSE)
@@ -21,46 +24,84 @@ library(sva)
 dataset = read.table(file='test-data/dnaDamage-test.txt', sep='\t', header = TRUE,strip.white=TRUE,stringsAsFactors=FALSE)
 
 
-#split original features matrix into two matrix, metadata and features
+# split original features matrix into two matrix, metadata and features
 
 metadata <- dataset %>% select (imageID,Control,screen,plate,tp,ch)
 features <- dataset %>% select (-imageID,-Control,-screen,-tp,-ch,-plate)
 
-#scale features matrix
-features_m <-as.matrix(scale(features))
+# function remove zero variance column
+removeZeroVar <-function(features){
+  features[, sapply(features,var) != 1e-5]  
+}
 
-#batch variable vector
-batch<- metadata %>% select(plate)
+features <- removeZeroVar(features)
 
-#print(batch)
+# remove columns with more than 5% NAs
+features <- features[, which(colMeans(!is.na(features))>0.05)]
 
-#features matrix used for combat function.
-sva_features <- t(features_m)
-
-batch = as.factor(batch$plate)
-
-#bind plate back to features matrix
-with_plate <- cbind(as.data.frame(features_m),batch)
-
-#assign plate as name
-#rownames(with_plate)[nrow(with_plate)] <- "batch"
-
-mod = model.matrix(~1,data=with_plate)
+# Standardise features
+features <-as.matrix(scale(features))
 
 
-#apply combat function
-combat_data = ComBat(dat=with_plate,batch=batch,mod=mod,par.prior=TRUE)
+# batch variable
+batch<- as.factor(metadata$plate)
 
-#export comabat_data
-#combat_data <- as.data.frame(t(combat_data)) %>% select (-batch)
-
-#write.table(cbind(metadata,combat_data),file="combat_features.txt", sep="\t", row.names = FALSE)
+# Create the model for combat,the data should have samples (i.e. images) in rows and features in columns
+mod = model.matrix(~1,data=data.frame(features))
 
 
+# apply combat function
+combat_data = ComBat(dat=t(features),batch=batch,mod=mod,par.prior=TRUE)
+
+# add metadata back
+combat_data <- data.frame(metadata,t(combat_data))
+
+# extract controls
+ctrl <- combat_data[!(combat_data$Control=="0"),]
+nctrl <- combat_data[!(combat_data$Control=="-"),]
+
+ctrl_features <-ctrl %>% select (-imageID,-Control,-screen,-tp,-ch,-plate)
+ctrl_metadata <-ctrl %>% select (imageID,Control,screen,tp,ch,plate)
+nctrl_features <-nctrl %>% select (-imageID,-Control,-screen,-tp,-ch,-plate)
+
+
+# PCA on controls
+pca_ctrl <- prcomp(t(ctrl_features),scale.=T, center=T)
+pca_eigenvalues <- pca_ctrl$sdev^2
+no_of_selected_features <- get_elbow_ofDistribution(1:length(pca_eigenvalues),pca_eigenvalues)
+
+screeplot(pca_ctrl)
+
+pca_nctrl <- prcomp(t(nctrl_features),scale.=T, center=T)
+
+predict(pca_ctrl,newdata = nctrl_features)
+
+
+# variance explained, first PC 21.5%, second 8%
+# summary(pca)$importance[2,]
+# var_explained <-pca$sdev^2/sum(pca$sdev^2)
+# var_explained [1:10]
+# pca$center[1:5]
+# pca$x[1:5,1:5]
+
+# plot using first 2 components for all
+# pca$x %>% 
+#   as.data.frame %>%
+#   ggplot(aes(x=PC1,y=PC2)) + geom_point(size=4) +
+#   theme_bw(base_size=32) + 
+#   labs(x=paste0("PC1: ",round(var_explained[1]*100,1),"%"),
+#        y=paste0("PC2: ",round(var_explained[2]*100,1),"%")) +
+#   theme(legend.position="top")
+
+
+#write.table(combat_data,file="combat_features.txt", sep="\t", row.names = FALSE)
 
 
 
 
+
+
+###non-controls
 
 
 
